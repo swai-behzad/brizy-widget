4273b749ec6971b94f6423ce7d4e5fc10df8db35
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2025-04-07 13:54:56 +0330

    new test

--- src/Textbox/index.ts
+++ src/Textbox/index.ts
@@ -10,47 +10,47 @@ Brizy.registerComponent({
     editor: TextboxEditor,
     view: TextboxView
   },
-//   options: {
-//     content: {
-//       type: "text",
-//       defaultValue: "Type something here..."
-//     },
-//     options: {
-//       content: {
-//         type: "text",
-//         label: "Text",
-//         defaultValue: "Type something here..."
-//       },
-//       typography: {
-//         type: "typography",
-//         label: "Typography",
-//         defaultValue: {}
-//       },
-//       color: {
-//         type: "colorPicker",
-//         label: "Text Color",
-//         defaultValue: "#000000"
-//       },
-//       padding: {
-//         type: "padding",
-//         label: "Padding",
-//         defaultValue: {
-//           top: "10px",
-//           right: "10px",
-//           bottom: "10px",
-//           left: "10px"
-//         }
-//       },
-//       shadow: {
-//         type: "textShadow",
-//         label: "Text Shadow",
-//         defaultValue: "none"
-//       },
-//       alignment: {
-//         type: "alignment",
-//         label: "Text Alignment",
-//         defaultValue: "left"
-//       }
-//     }
-//   }
+  options: {
+    content: {
+      type: "text",
+      defaultValue: "Type something here..."
+    }
+    //  options: {
+    //    content: {
+    //      type: "text",
+    //      label: "Text",
+    //      defaultValue: "Type something here..."
+    //    },
+    //    typography: {
+    //      type: "typography",
+    //      label: "Typography",
+    //      defaultValue: {}
+    //    },
+    //    color: {
+    //      type: "colorPicker",
+    //      label: "Text Color",
+    //      defaultValue: "#000000"
+    //    },
+    //    padding: {
+    //      type: "padding",
+    //      label: "Padding",
+    //      defaultValue: {
+    //        top: "10px",
+    //        right: "10px",
+    //        bottom: "10px",
+    //        left: "10px"
+    //      }
+    //    },
+    //    shadow: {
+    //      type: "textShadow",
+    //      label: "Text Shadow",
+    //      defaultValue: "none"
+    //    },
+    //    alignment: {
+    //      type: "alignment",
+    //      label: "Text Alignment",
+    //      defaultValue: "left"
+    //    }
+    //  }
+  }
 });
