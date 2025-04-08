01de757fb08529e341ef59883f9f404bfbcbbf49
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2025-04-08 11:51:30 +0330

    test props

--- src/Input/Editor/index.tsx
+++ src/Input/Editor/index.tsx
@@ -1,15 +1,26 @@
 import { JSX } from "react";
 
-interface Props {
-   value: string;
-   onChange: (v: string) => void;
- }
+// interface Props {
+//   value: string;
+// }
 
-export const InputEditor = (props: Props): JSX.Element => {
-   const { value, onChange } = props;
-   return (
-     <div className="brz-input">
-       <input type="text" value={value} onChange={({ target: { value } }): void => onChange(value)} />
-     </div>
-   );
- };
+export const InputEditor = (props: any): JSX.Element => {
+  console.log(props);
+
+  const { value } = props;
+
+  return (
+    <div className="brz-input">
+      <input
+        type="text"
+        value={value}
+        onChange={() => console.log("changed", value)}
+      />
+      <button
+        onClick={() => {
+          console.log(props);
+        }}
+      ></button>
+    </div>
+  );
+};
