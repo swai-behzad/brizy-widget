8939f0c77d91abf88216db9fc11339c11a7ef594
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2025-04-08 14:16:20 +0330

    change map

--- src/Map/Editor/index.tsx
+++ src/Map/Editor/index.tsx
@@ -1,18 +1,25 @@
 import { ReactElement } from "react";
 import { Props } from "../types";
 
-export const Editor = (props: Props): ReactElement => {
-  const { address, zoom = 13, height, widthSuffix } = props;
-  const URL = "https://www.google.com/maps/embed/v1/place";
-  const KEY = "AIzaSyCcywKcxXeMZiMwLDcLgyEnNglcLOyB_qw";
-  const iframeSrc = `${URL}?key=${KEY}&q=${address}&zoom=${zoom}`;
+export const Editor = (props: any): ReactElement => {
+  //   const { address, zoom = 13, height, widthSuffix } = props;
+//   const URL = "https://www.google.com/maps/embed/v1/place";
+//   const KEY = "AIzaSyCcywKcxXeMZiMwLDcLgyEnNglcLOyB_qw";
+//   const iframeSrc = `${URL}?key=${KEY}&q=${address}&zoom=${zoom}`;
 
   return (
     <div
       className="mapThirdComponent"
-      style={{ pointerEvents: "none", height: `${height}${widthSuffix}` }}
+      // style={{ pointerEvents: "none", height: `${height}${widthSuffix}` }}
     >
-      <iframe src={iframeSrc} title="map" />
+      <button
+        onClick={() => {
+          console.log(props);
+        }}
+      >
+        click
+      </button>
+      {/* <iframe src={iframeSrc} title="map" /> */}
     </div>
   );
 };
