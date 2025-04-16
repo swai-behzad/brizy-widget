d094eb9ed77266e7ee982be2bb59692b1896f137
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2025-04-16 10:07:19 +0330

    new test

--- build/tools/CustomTool/register.js
+++ build/tools/CustomTool/register.js
@@ -1,30 +1,29 @@
+const React = window.unlayer.React;
+
+const Viewer = () => {
+   return <div>I am a custom tool.</div>;
+};
+
 unlayer.registerTool({
-   name: 'my_tool',
-   label: 'My Tool',
-   icon: 'fa-smile',
-   supportedDisplayModes: ['web', 'email'],
-   options: {},
-   values: {},
-   renderer: {
-     Viewer: unlayer.createViewer({
-       render(values) {
-         return '<div>I am a custom tool.</div>';
-       },
-     }),
-     exporters: {
-       web: function (values) {
-         return '<div>I am a custom tool.</div>';
-       },
-       email: function (values) {
-         return '<div>I am a custom tool.</div>';
-       },
-     },
-     head: {
-       css: function (values) {},
-       js: function (values) {},
-     },
-   },
-   validator(data) {
-     return [];
-   },
- });
+  name: 'my_tool',
+  label: 'My Tool',
+  icon: 'fa-smile',
+  supportedDisplayModes: ['web', 'email'],
+  options: {},
+  values: {},
+  renderer: {
+    Viewer: Viewer, // our React Viewer
+    exporters: {
+      web: function (values) {
+        return '<div>I am a custom tool.</div>';
+      },
+      email: function (values) {
+        return '<div>I am a custom tool.</div>';
+      },
+    },
+    head: {
+      css: function (values) {},
+      js: function (values) {},
+    },
+  },
+});
